f7dc17dedf9d56c1d7c07474d3e89a2c93de0d75
MechaDragonX <theofficialrv@gmail.com>
2017-12-01 15:48:51 -0800

    Initialization

--- src/org/usfirst/frc/team2976/robot/Robot.java
+++ src/org/usfirst/frc/team2976/robot/Robot.java
@@ -14,6 +14,8 @@ import org.usfirst.frc.team2976.robot.subsystems.DriveTrain;
 import org.usfirst.frc.team2976.robot.subsystems.ExampleSubsystem;
 import org.usfirst.frc.team2976.robot.subsystems.Roller;
 
+//Raghav was here...
+
 /**
  * The VM is configured to automatically run this class, and to call the
  * functions corresponding to each mode, as described in the IterativeRobot
